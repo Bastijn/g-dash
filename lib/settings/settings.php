20572fda857330dcb3d25ecfa524af8a8d81246a
Bastijn <6638869+Bastijn@users.noreply.github.com>
2018-02-03 12:58:15 +0100

    Update to version 0.23
    Update the settings file to 0.23 to send the update out to the public

--- lib/settings/settings.php
+++ lib/settings/settings.php
@@ -1,5 +1,5 @@
 <?php $GDASH = array (
-  'currentversion' => '0.22',
+  'currentversion' => '0.23',
   'updatecheck' => 'http://g-dash.nl/autoupdate.php',
   'updatelocation' => 'http://g-dash.nl/download/'
 ); ?>
