c567200f2cdcafbad1d3e513ecb0869878cd7b96
Bastijn <6638869+Bastijn@users.noreply.github.com>
2018-07-16 09:36:32 +0200

    Add point release to settings file
    Point release to update for JS caching

--- lib/settings/settings.php
+++ lib/settings/settings.php
@@ -1,5 +1,5 @@
 <?php $GDASH = array (
-  'currentversion' => '1.0',
+  'currentversion' => '1.01',
   'updatecheck' => 'https://g-dash.nl/autoupdate.php',
   'updatelocation' => 'https://g-dash.nl/download/',
   'nlgrate' => 
